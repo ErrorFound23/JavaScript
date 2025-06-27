8363655ad057d7ee286b81d6d85fdb5ee1715c8a
Yash Rana <ranayash35495@gmail.com>
2025-06-27 13:18:10 +0000

    conversion behaviour

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "true"
+let score = "true";
 
 // console.log(typeof score)
 // console.log(typeof(score))
@@ -9,11 +9,11 @@ let valueInNumber = Number(score);
 
 // "33" => 33
 // "33abc" => NaN
-// true => 1; false => 0 
+// true => 1; false => 0
 
 let isLoggedIn = "yash";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn);
 // console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 =>  false
@@ -22,6 +22,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// ********************* Operation ***************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "Yash";
+let str2 = " rana";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2; // never do this thing
+
+let gameCounter = 100;
+
+console.log(gameCounter++); // postfix increment
+console.log(++gameCounter); // prefix increment

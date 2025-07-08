4e8a000ea8f3009a817a9fe92d9abeba963dd52f
Yash Rana <ranayash35495@gmail.com>
2025-07-08 08:30:25 +0000

    while and do...while loop

---
+++ 05_iterations/02_while_doWhile_loop.js
@@ -0,0 +1,25 @@
+// while loop
+
+// while (condition) {
+    
+// }
+
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2;
+// }
+
+const myArray = ["Flash", "Batman", "Superman"]
+let index = 0;
+
+while (index < myArray.length) {
+    // console.log(`Value is ${myArray[index]}`);
+    index ++;
+}
+
+let score = 11;
+do {
+    console.log(`Score is ${score}`);
+    score ++;
+} while (score <= 10);
